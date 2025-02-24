ceab5666152cce1da3fb8546203063268b0cb00f
nayangodhwani-atri <ngodhwan@atrihub.io>
2025-02-24 10:18:22 -0800

    feat: Created Database
    [ICTRAINING-1856]

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'djangoTutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "djangodb",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
